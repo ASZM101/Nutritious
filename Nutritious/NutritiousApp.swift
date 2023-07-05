7652bb0885a403e20ad08f86c7963315dfc25f8d
ASZM101 <adrianasofia.zambrano@gmail.com>
2023-07-05 13:05:54 -0500

    Initial Commit

---
+++ Nutritious/NutritiousApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutritiousApp.swift
+//  Nutritious
+//
+//  Created by Adriana Zambrano on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct NutritiousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
